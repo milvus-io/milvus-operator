c5f501cd75cf293c0d42cece99113bad8835af35
Weida Zhu <weida.zhu@zilliz.com>
2021-09-10 13:55:39 +0800

    update rbac file
    Signed-off-by: Weida Zhu <weida.zhu@zilliz.com>

--- controllers/milvuscluster_controller.go
+++ controllers/milvuscluster_controller.go
@@ -58,6 +58,12 @@ func NewMilvusClusterReconciler(client client.Client, scheme *runtime.Scheme) *M
 //+kubebuilder:rbac:groups=milvus.io,resources=milvusclusters,verbs=get;list;watch;create;update;patch;delete
 //+kubebuilder:rbac:groups=milvus.io,resources=milvusclusters/status,verbs=get;update;patch
 //+kubebuilder:rbac:groups=milvus.io,resources=milvusclusters/finalizers,verbs=update
+//+kubebuilder:rbac:groups=apps,resources=statefulsets;deployments,verbs=get;list;watch;create;update;patch;delete
+//+kubebuilder:rbac:groups="",resources=pods,verbs=get;list;watch;create;update;patch;delete
+//+kubebuilder:rbac:groups="",resources=persistentvolumeclaims,verbs=get;list;watch;create;update;patch;delete
+//+kubebuilder:rbac:groups="",resources=services,verbs=get;list;watch;create;update;patch;delete
+//+kubebuilder:rbac:groups="",resources=configmaps,verbs=get;list;watch;create;update;patch;delete
+//+kubebuilder:rbac:groups="",resources=sercrets,verbs=get;list;watch;create;update;patch;delete
 
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
