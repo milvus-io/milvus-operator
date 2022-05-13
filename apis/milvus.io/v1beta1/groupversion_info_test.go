8237db8233a377d0cde26d76cb0357b089b4bd50
shaoyue.chen <shaoyue.chen@zilliz.com>
2022-05-13 09:30:17 +0800

    Add v1beta1 model
    Signed-off-by: shaoyue.chen <shaoyue.chen@zilliz.com>

---
+++ apis/milvus.io/v1beta1/groupversion_info_test.go
@@ -0,0 +1,13 @@
+package v1beta1
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestResource(t *testing.T) {
+	ret := Resource("test")
+	assert.Equal(t, "milvus.io", ret.Group)
+	assert.Equal(t, "test", ret.Resource)
+}
