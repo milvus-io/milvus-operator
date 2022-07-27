5c0a2c7fea3c4bf0ba5d1beb399f9d1b5b797f31
shaoyue.chen <shaoyue.chen@zilliz.com>
2022-07-27 16:50:09 +0800

    Update Milvus to V2.1.0, milvus-helm to 3.1.2, bump version
    Signed-off-by: shaoyue.chen <shaoyue.chen@zilliz.com>

--- test/upgrade.sh
+++ test/upgrade.sh
@@ -8,8 +8,11 @@ kubectl --timeout 20m wait --for=condition=MilvusReady milvus my-release
 echo "Deploying upgrade"
 helm -n milvus-operator upgrade --wait --timeout 10m --reuse-values --set image.repository=milvus-operator,image.tag=sit milvus-operator ./charts/milvus-operator
 sleep 60
+kubectl get pods
 kubectl --timeout 10m wait --for=condition=MilvusReady milvus my-release
 # check dependencies no revision change
+helm list
 helm list |grep -v NAME |awk '{print $3}' | xargs -I{} [ {} -eq "1" ]
 # check milvus pods no restart
+kubectl get pods
 kubectl get pods |grep -v NAME |awk '{print $4}' | xargs -I{} [ {} -eq "0" ]
