5b77bc47cfb511cb19995c0eb16242f7895506d8
shaoyue <shaoyue.chen@zilliz.com>
2022-12-15 18:39:19 +0800

    Upgrade milvus to 2.2.1, helm values to 3.3.6, bump version (#206)
    Signed-off-by: shaoyue.chen <shaoyue.chen@zilliz.com>
    
    Signed-off-by: shaoyue.chen <shaoyue.chen@zilliz.com>

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -8,7 +8,7 @@ import (
 )
 
 const (
-	DefaultMilvusVersion   = "v2.2.0"
+	DefaultMilvusVersion   = "v2.2.1"
 	DefaultMilvusBaseImage = "milvusdb/milvus"
 	DefaultMilvusImage     = DefaultMilvusBaseImage + ":" + DefaultMilvusVersion
 	DefaultImagePullPolicy = corev1.PullIfNotPresent
