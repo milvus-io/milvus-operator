f45cf17917eb07244f0a610363b6c22f26dbefb7
shaoyue <shaoyue.chen@zilliz.com>
2023-04-14 14:56:24 +0800

    kafka probe add retry (#239)
    Signed-off-by: shaoyue.chen <shaoyue.chen@zilliz.com>

--- pkg/external/kafka.go
+++ pkg/external/kafka.go
@@ -2,6 +2,7 @@ package external
 
 import (
 	"context"
+	"log"
 	"time"
 
 	"github.com/pkg/errors"
@@ -18,8 +19,26 @@ func CheckKafka(brokerList []string) error {
 		Topic:   "_milvus-operator",
 	})
 	defer r.Close()
-	ctx, cancel := context.WithTimeout(context.Background(), time.Second*5)
-	defer cancel()
-	err := r.SetOffsetAt(ctx, time.Now())
-	return errors.Wrap(err, "check consume offset from borker failed")
+	var checkKafka = func() error {
+		ctx, cancel := context.WithTimeout(context.Background(), time.Second*5)
+		defer cancel()
+		err := r.SetOffsetAt(ctx, time.Now())
+		return errors.Wrap(err, "check consume offset from borker failed")
+	}
+	const backOffInterval = time.Second * 1
+	const maxRetry = 3
+	return doWithBackoff("checkKafka", checkKafka, maxRetry, backOffInterval)
+}
+
+func doWithBackoff(name string, fn func() error, maxRetry int, backOff time.Duration) error {
+	var err error
+	for i := 0; i < maxRetry; i++ {
+		err = fn()
+		if err == nil {
+			return nil
+		}
+		log.Printf("%s with backoff failed, retry %d, err: %v\n", name, i, err)
+		time.Sleep(backOff)
+	}
+	return errors.Wrap(err, name+" with backoff failed")
 }
